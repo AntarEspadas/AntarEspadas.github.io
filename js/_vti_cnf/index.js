9c9e5dda51ca2fd81b01e122227d6b559af2fe9a
Naratna <59068049+Naratna@users.noreply.github.com>
2020-05-03 16:28:46 -0500

    Add files via upload

--- js/_vti_cnf/index.js
+++ js/_vti_cnf/index.js
@@ -1,10 +1,10 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|Flor-PC\\Antar
 vti_modifiedby:SR|Flor-PC\\Antar
-vti_timelastmodified:TW|03 May 2020 19:15:16 -0000
+vti_timelastmodified:TR|03 May 2020 21:19:51 -0000
 vti_timecreated:TR|02 May 2020 18:40:21 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|index.html
-vti_nexttolasttimemodified:TW|03 May 2020 19:15:16 -0000
-vti_cacheddtm:TX|03 May 2020 19:15:16 -0000
-vti_filesize:IR|675
+vti_nexttolasttimemodified:TW|03 May 2020 21:17:30 -0000
+vti_cacheddtm:TX|03 May 2020 21:19:51 -0000
+vti_filesize:IR|2287
--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 ﻿function toggleSidebar(){
 	if(document.getElementById('sidebar').className=="sidebar-hidden"){
-		var box = document.createElement("div");		box.id = "sidebar-outside";
+		var box = document.createElement("div");
+		box.id = "sidebar-outside";
 		box.addEventListener("click",toggleSidebar)
 		document.body.appendChild(box);
 
@@ -14,6 +15,53 @@
 	}
 }
 
-function showLightbox(resultBox){
-	docment.getElementById('lightbox').className="lightbox"
-	}
+function showLightbox(){
+		var box = document.createElement("div");
+		box.id = "lightbox";
+		box.addEventListener("click",hideLightbox);
+		document.body.appendChild(box);
+		
+		var image = document.createElement("img");
+		console.log(this);
+		image.src = this.children[1].src;
+		image.id = "lightbox-image";
+		document.body.appendChild(image);
+		}
+	
+function hideLightbox(){
+		var box = document.getElementById("lightbox");
+		document.body.removeChild(box);
+		
+		var image = document.getElementById("lightbox-image");
+		document.body.removeChild(image);
+}
+
+function addResult(imageAdress, informationLabels, informationElements){
+		var resultElement = document.createElement("div");
+		resultElement.className = "result-element";
+		resultElement.addEventListener = ("click", showLightbox, false);
+		document.getElementById("results").appendChild(resultElement);
+
+		var resultInfo = document.createElement("div");
+		resultInfo.className = "result-info";
+		resultElement.appendChild(resultInfo);
+
+		for (let index = 0, info; index < informationLabels.length; index++) {
+			info = document.createElement("p");
+			info.textContent = informationLabels[index] + ": " + informationElements[index];
+			resultInfo.appendChild(info);
+		}
+
+		var image = document.createElement("img");
+		image.className = "result-image";
+		image.src = imageAdress;
+		resultElement.appendChild(image);
+}
+
+function addResultTest(){
+		var imageAdress = "images/000___1582350169.png";
+		var informatoinLabels = ["Title", "Chapter", "Page"];
+		var informatoinElements = ["Chika Fujiwara wants to make it swell", "91", "1"];
+
+		addResult(imageAdress, informatoinLabels, informatoinElements)
+}
