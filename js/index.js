17525940e66c797f30a40e7135556c88a5d50c7b
Naratna <59068049+Naratna@users.noreply.github.com>
2020-05-03 14:20:21 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+﻿function toggleSidebar(){
+	if(document.getElementById('sidebar').className=="sidebar-hidden"){
+		var box = document.createElement("div");		box.id = "sidebar-outside";
+		box.addEventListener("click",toggleSidebar)
+		document.body.appendChild(box);
+
+		document.getElementById('sidebar').className="sidebar";	}
+	else{
+		var box = document.getElementById("sidebar-outside");
+		document.body.removeChild(box)
+		
+		document.getElementById('sidebar').className="sidebar-hidden";
+		//document.getElementById('sidebar-outside').className="sidebar-outside-hidden";
+	}
+}
+
+function showLightbox(resultBox){
+	docment.getElementById('lightbox').className="lightbox"
+	}
