20cb09a02ed79acfea8a91d0b6e6bcd0cfc11296
Naratna <59068049+Naratna@users.noreply.github.com>
2020-05-03 20:55:00 -0500

    Add files via upload

--- js/_vti_cnf/index.js
+++ js/_vti_cnf/index.js
@@ -1,10 +1,10 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|Flor-PC\\Antar
 vti_modifiedby:SR|Flor-PC\\Antar
-vti_timelastmodified:TR|03 May 2020 21:19:51 -0000
+vti_timelastmodified:TR|03 May 2020 23:25:01 -0000
 vti_timecreated:TR|02 May 2020 18:40:21 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|index.html
-vti_nexttolasttimemodified:TW|03 May 2020 21:17:30 -0000
-vti_cacheddtm:TX|03 May 2020 21:19:51 -0000
-vti_filesize:IR|2287
+vti_nexttolasttimemodified:TW|03 May 2020 23:24:42 -0000
+vti_cacheddtm:TX|03 May 2020 23:25:01 -0000
+vti_filesize:IR|2294
--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-﻿function toggleSidebar(){
+﻿var hideLightboxDone = true;
+
+function toggleSidebar(){
 	if(document.getElementById('sidebar').className=="sidebar-hidden"){
 		var box = document.createElement("div");
 		box.id = "sidebar-outside";
@@ -11,7 +13,6 @@
 		document.body.removeChild(box)
 		
 		document.getElementById('sidebar').className="sidebar-hidden";
-		//document.getElementById('sidebar-outside').className="sidebar-outside-hidden";
 	}
 }
 
@@ -22,24 +23,46 @@ function showLightbox(){
 		document.body.appendChild(box);
 		
 		var image = document.createElement("img");
-		console.log(this);
 		image.src = this.children[1].src;
 		image.id = "lightbox-image";
 		document.body.appendChild(image);
 		}
 	
 function hideLightbox(){
+		if(!hideLightboxDone){
+			return;
+		}
+		hideLightboxDone = false;
 		var box = document.getElementById("lightbox");
-		document.body.removeChild(box);
+		box.style.opacity = "0";
+		box.animate([
+			// keyframes
+			{ opacity: '0.75' }, 
+			{ opacity: '0' }
+		  ], { 
+			// timing options
+			duration: 250
+		  });
+		setTimeout(() => { document.body.removeChild(box); }, 250);
 		
 		var image = document.getElementById("lightbox-image");
-		document.body.removeChild(image);
+		image.style.opacity = "0";
+		image.animate([
+			// keyframes
+			{ opacity: '1' }, 
+			{ opacity: '0' }
+		  ], { 
+			// timing options
+			duration: 250
+		  });
+		setTimeout(() => { document.body.removeChild(image); }, 250);
+		setTimeout(() => { hideLightboxDone = true; }, 251);
 }
 
 function addResult(imageAdress, informationLabels, informationElements){
 		var resultElement = document.createElement("div");
 		resultElement.className = "result-element";
-		resultElement.addEventListener = ("click", showLightbox, false);
+		resultElement.addEventListener("click", showLightbox, false);
 		document.getElementById("results").appendChild(resultElement);
 
 		var resultInfo = document.createElement("div");
@@ -56,6 +79,23 @@ function addResult(imageAdress, informationLabels, informationElements){
 		image.className = "result-image";
 		image.src = imageAdress;
 		resultElement.appendChild(image);
+		
+		
+		}
+
+function clearResults(){
+		var results = document.getElementsByClassName("result-element");
+		var loops = results.length;
+		for (let index = 0; index < loops; index++) {
+			document.getElementById("results").removeChild(results[0]);
+		}
+}
+
+function searchBarKeyDown(event) {
+	if(event.keyCode === 13){
+		clearResults()
+		batchAddResultTest(7, 250);
+	}
 }
 
 function addResultTest(){
@@ -64,4 +104,11 @@ function addResultTest(){
 		var informatoinElements = ["Chika Fujiwara wants to make it swell", "91", "1"];
 
 		addResult(imageAdress, informatoinLabels, informatoinElements)
+}
+
+
+function batchAddResultTest(amount, delay){
+	for (let index = 0; index < amount; index++) {
+		setTimeout(() => { addResultTest(); }, delay*index);
+	}
 }
